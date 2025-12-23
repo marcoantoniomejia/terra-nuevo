bbd1f3062626bf9a3e07be5cbd89c6028e0e2847
Marco Mejia <marcoantonio.mejialara@gmail.com>
2025-12-22 19:18:52 -0600

    nuevos cambios ahora si

--- modules/cloud-sql/main.tf
+++ modules/cloud-sql/main.tf
@@ -1,8 +1,12 @@
+locals {
+  peering_range = var.psa_peering_range_name != "" ? var.psa_peering_range_name : google_compute_global_address.private_ip_address[0].name
+}
+
 resource "google_sql_database_instance" "instance" {
   project          = var.project_id
   name             = var.instance_name
   database_version = var.database_version
-  region           = var.subnets[var.subnet_name].region
+  region           = substr(var.zone, 0, length(var.zone) - 2)
 
   settings {
     tier = var.tier
@@ -23,18 +27,19 @@ resource "google_project_service" "private_service_access" {
 }
 
 resource "google_compute_global_address" "private_ip_address" {
-  project       = var.subnets[var.subnet_name].project
-  name          = "private-ip-for-sql"
+  count         = var.psa_peering_range_name == "" ? 1 : 0
+  project       = var.host_project_id
+  name          = "private-ip-for-sql-${var.instance_name}"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
-  prefix_length = 16
+  prefix_length = 20 # As per user request
   network       = var.network_self_link
 }
 
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = var.network_self_link
   service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+  reserved_peering_ranges = [local.peering_range]
 
   depends_on = [google_project_service.private_service_access]
 }
--- modules/cloud-sql/variables.tf
+++ modules/cloud-sql/variables.tf
@@ -37,3 +37,14 @@ variable "network_self_link" {
   description = "El self-link de la red."
   type        = string
 }
+
+variable "psa_peering_range_name" {
+  description = "El nombre del rango de peering para la conexión de servicios privados. Si se deja en blanco, se creará uno nuevo."
+  type        = string
+  default     = ""
+}
+
+variable "host_project_id" {
+  description = "El ID del proyecto anfitrión donde se encuentra la VPC compartida."
+  type        = string
+}
